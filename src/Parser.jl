eb0da04318e3c1e0679c2c364ad2585641685475
Lucas Baussay <lucas.baussay.lb@gmail.com>
2020-09-04 23:31:24 +0200

    Begin of the Parser

--- src/Parser.jl
+++ src/Parser.jl
@@ -1,3 +1,12 @@
 function parser(m::JuMP.Model)
+    variable_list = JuMP.all_variables(m)
+    @assert all(v -> JuMP.has_lower_bound(v) && JuMP.has_upper_bound(v), variable_list) "All variables must be bounded"
 
+    for var in variable_list
+        lower_bound = JuMP.lower_bound(var)
+        upper_bound = JuMP.upper_bound(var)
+
+        scale = upper_bound - lower_bound
+
+        num_created_city::Int64 = Int(ceil(log2(scale)))
 end
