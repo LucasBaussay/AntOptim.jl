3107e267eefe35c7a5f638d43dd9b95d7c93fb70
Lucas Baussay <lucas.baussay.lb@gmail.com>
2020-09-12 16:35:39 +0200

    Dm

---
+++ test/Dm/SppGenerator.jl
@@ -0,0 +1,93 @@
+import Random
+
+struct VariableIndex
+    value::Int64
+end
+
+struct ConstraintIndex
+    value::Int64
+end
+
+const CI = ConstraintIndex
+const VI = VariableIndex
+
+function chooseNbVar(L::Vector{Float64})
+    x::Float64 = rand()
+
+    if x < L[1]
+        return 2
+    elseif x+L[1] < L[2]
+        return 3
+    else
+        return 4
+    end
+end
+
+# Idée comme structure de donnée : comme données en entrée !
+
+function generate(nbrVar::Int64, nbrConst::Int64)
+    c::Dict{VI, Int64} = Dict( VI(ind) => Random.rand(1:28) for ind in 1:nbrVar)
+    # Mbis::Array{Int64, 2} = zeros(Int, nbrConst, nbrVar)
+    # Mtris::Dict{VI, Dict{VI, Int64}} = Dict(VI(var) => Dict(VI(varBis) => 0 for varBis = 1:nbrVar) for var = 1:nbrVar)
+
+    M::Dict{CI, Vector{VI}} = Dict()
+
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # La complexité de ca !
+    # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    # listOccur::Dict{VI, Dict{VI, Int64}} = Dict(VI(var) => Dict(VI(varBis) => 0 for varBis = 1:nbrVar) for var = 1:nbrVar)
+
+    listOccur::Dict{VI, Dict{VI, Int64}} = Dict()
+    listLiaison::Dict{VI, Vector{VI}} = Dict()
+
+    occurVar::Dict{VI, Int64} = Dict(VI(var) => 0 for var = 1:nbrVar)
+
+    listProba::Vector{Float64} = [0.75, 0.20, 0.05]
+
+    for ind in 1:nbrConst
+        varConst::Int64 = chooseNbVar(listProba)
+        ListTirage = []
+        for _ in 1:varConst
+            var = rand(1:nbrVar)
+            while var in ListTirage
+                var = rand(1:nbrVar)
+            end
+            push!(ListTirage, var)
+            # Mbis[ind, var] = 1
+        end
+        # println(ListTirage)
+        for var1 in VI.(ListTirage)
+            occurVar[var1] += 1
+            for var2 in VI.(ListTirage)
+                if var1 != var2
+                    if !haskey(listOccur, var1)
+                        push!(listOccur, var1 => Dict())
+                        push!(listLiaison, var1 => [var1])
+                    end
+                    if !haskey(listOccur[var1], var2)
+                        push!(listOccur[var1], var2 => 0)
+                        push!(listLiaison[var1], var2)
+                    end
+                    listOccur[var1][var2] += 1
+
+                end
+            end
+        end
+
+        push!(M, CI(ind) => VI.(ListTirage))
+    end
+    return c, M, listOccur, listLiaison, occurVar
+    # for indVar = 1:nbrVar
+    #     for indConst = 1:nbrConst
+    #         if Mbis[indConst, indVar] == 1
+    #             for indVarBis = 1:nbrVar
+    #                 if Mbis[indConst, indVarBis] == 1 && indVar != indVarBis
+    #                     Mtris[VI(indVar)][VI(indVarBis)]+=1
+    #                 end
+    #             end
+    #         end
+    #     end
+    # end
+    # return c, M, Mbis
+end
---
+++ test/Dm/glouton.jl
@@ -0,0 +1,80 @@
+include("SppGenerator.jl")
+#
+# function profFunction(var::Int64, c::Vector{Int64}, M::Array{Int64, 2})
+#     return c[var]/sum(M[:,var])
+# end
+
+# function findVariable(c::Vector{Int64}, M::Array{Int64, 2}, nbVar::Int64)
+#     varMax = 1
+#     maxProf = profFunction(varMax, c, M)
+#     for var = 2:nbVar
+#         prof = profFunction(var, c, M)
+#         if prof > maxProf
+#             varMax = var
+#             maxProf = prof
+#         end
+#     end
+#     varMax = maxProf == 0 ? -1 : varMax
+#     return varMax
+# end
+
+# function max(dict::Dict{Any, Int64})
+#     valMax = -Inf
+#     varMax = -1
+#     for pair in dict
+#         if pair.second > valMax
+#             varMax = pair.first
+#             valMax = pair.second
+#         end
+#     end
+#     return (indMax, valMax)
+# end
+
+function profFunction(var::VI, c::Dict{VI, Int64}, occur::Dict{VI, Int64})
+    return (haskey(c, var) && haskey(occur, var) ? c[var]/occur[var] : -Inf)
+end
+
+function findVariable(c::Dict{VI, Int64}, occurVar::Dict{VI, Int64}, variables::Vector{VI})
+    varMax = VI(-1)
+    valMax = typemin(Float64)
+    for var in variables
+
+        val = profFunction(var, c, occurVar)
+        if val > valMax
+            varMax = var
+            valMax = val
+        end
+    end
+    return varMax
+end
+
+function z(x::Dict{VI, Int64}, c::Dict{VI, Int64})
+    val::Int64 = 0
+    for (x_i, b) in x
+        val += b*c[x_i]
+    end
+    return val
+end
+
+function heuristic(c::Dict{VI, Int64}, M::Dict{CI, Vector{VI}}, listOccur::Dict{VI, Dict{VI, Int64}}, listLiaison::Dict{VI, Vector{VI}}, occurVar::Dict{VI, Int64})
+    nbVar = length(c)
+    nbConst = length(M)
+    x0::Dict{VI, Int64} = Dict(var => 0 for var=VI.(1:nbVar))
+    # relationList::Vector{Vector{Int64}} = Vector{Vector{Int64}}(undef, nbrVar)
+    variables = VI.(1:nbVar)
+    while variables != []
+        x_i = findVariable(c, occurVar, variables)
+        filter!(x -> !(x in listLiaison[x_i]), variables)
+        x0[x_i] = 1
+        # Reduire OccurVar
+    end
+    return x0, z(x0, c)
+end
+
+function heuristic(nbVar::Int64, nbConst::Int64)
+    c, M, listOccur, listLiaison, occurVar = generate(nbVar, nbConst)
+    return heuristic(c, M, listOccur, listLiaison, occurVar)
+end
+
+
+    # Faux ! Ecrit ton heuristique sur papier avant de la coder !
