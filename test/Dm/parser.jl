e1c0eac73debe176ecdda2d5631314655199f79f
Lucas Baussay <lucas.baussay.lb@gmail.com>
2020-09-18 23:31:25 +0200

    Dm

--- test/Dm/parser.jl
+++ test/Dm/parser.jl
@@ -1,11 +1,41 @@
 struct Problem
-    C::Vector{Int64}
-    A::Union{Vector{Vector{Int64}}, Array{Int64, 2}}
+    C::Vector{Int64} # Vecteur des pofits de chaque variables
+    A::Union{Vector{Vector{Int64}}, Array{Int64, 2}} # Matrice creuse/Liste d'adgacence des contraintes
+
+    #Donées annexes
+
+    listOccur::Array{Int64, 2}
+    occurVar :: Vector{Int64} # Nombre de variable que chaque variable penalise
+    occurConst::Vector{Int64} #Nombre de fois qu'une variable apparait  dans une contraintes
 end
 
 struct Solution
-    x::BitArray
+    x::Vector{Int64}
     z::Int64
+
+	# Données annexes
+
+	var1::Vector{Int64} # Indice des variables misent à 1
+	var0::Vector{Int64} # Indice des variables misent à 0
+end
+
+function Solution(x::Vector{Int64}, z::Int64)
+	num1 = sum(x)
+	var1 = Vector{Int64}(undef, num1)
+	var0 = Vector{Int64}(undef, length(x)-num1)
+
+	num0 = 0
+	num1 = 0
+	for ind = 1:length(x)
+		if x[ind] == 0
+			num0 += 1
+			var0[num0] = ind
+		else
+			num1 += 1
+			var1[num1] = ind
+		end
+	end
+	return Solution(x, z, var1, var0)
 end
 
 function loadSPP(fname)
@@ -16,11 +46,16 @@ function loadSPP(fname)
     C = parse.(Int, split(readline(f)) )
     # lecture des m contraintes et reconstruction de la matrice binaire A
     #A=Vector{Vector{Int64}}(undef, m )
-    A = Array{Int64, 2}(undef, m, n)
+    # A = Array{Int64, 2}(undef, m, n)
+    A = zeros(Int64, m, n)
+    listOccur = zeros(Int64, n, n)
+    occurVar = zeros(Int64, n)
+    occurConst = zeros(Int64, n)
+
     for i=1:m
         # lecture du nombre d'elements non nuls sur la contrainte i (non utilise)
-        nbVarConst = parse(Int, readline(f))
-        A[i] = Vector{Int64}(undef, nbVarConst)
+        nbVarConst = parse.(Int, split(readline(f)))
+        # A[i] = Vector{Int64}(undef, nbVarConst[1])
         # lecture des indices des elements non nuls sur la contrainte i
         line = parse.(Int, split(readline(f)))
         # for ind in 1:nbVarConst
@@ -28,24 +63,35 @@ function loadSPP(fname)
         # end
         for var in line
             A[i, var] = 1
+            occurConst[var] += 1
+            for var2 in line
+                listOccur[var, var2] = 1
+                occurVar[var] += 1
+            end
         end
     end
     close(f)
-    return Problem(C, A)
+    return Problem(C, A, listOccur, occurVar, occurConst)
 end
 
-function calculUtil(var::Int64, problem::Problem)
-    prof::Int64 = problem.C[var]
-    nbOccur::Int64 = sum(problem.A[:, var])
-    return prof/(nbOccur)^0.5
+function calculUtil(var::Int64, problem::Problem, x::Vector{Int64})
+	util = undef
+	if x[var] != -1
+		util = -Inf
+	else
+		prof::Int64 = problem.C[var]
+		nbOccur::Int64 = problem.occurConst[var]
+		util = prof/(nbOccur)^0.5
+	end
+	return util
 end
 
 
-function findVariable(variables::Vector{Int64}, problem::Problem)::Int64
+function findVariable(x::Vector{Int64}, problem::Problem)::Int64
     var0 = -1
     utilMax = -Inf
-    for var in variables
-        util = calculUtil(var, problem)
+    for var = 1:length(x)
+        util = calculUtil(var, problem, x)
         if util > utilMax
             var0 = var
             utilMax = util
@@ -58,13 +104,124 @@ function gloutonBasique(fname)
     problem = loadSPP(fname)
     nbVar::Int64 = length(problem.C)
 
-    variables::Vector{Int64} = 1:nbVar
-
-    x::BitArray = BitArray(undef, nbVar)
+    x::Vector{Int64} = zeros(Int64, nbVar)
+    for ind = 1:nbVar
+    	x[ind] = -1
+    end
     z::Int64 = 0
 
-    nbVariableLibre = length(variables)
-    while nbVariableLibre != 0
-        x_i = findVariable(variables, problem)
+	nbVariablesLibre = nbVar
+
+    while nbVariablesLibre > 0
+        x_i = findVariable(x, problem)
         x[x_i] = 1
-        filterList = 
+        nbVariablesLibre -= 1
+        z += problem.C[x_i]
+        filterList = []
+        for ind = 1:nbVar
+            if problem.listOccur[x_i, ind] == 1 && x[ind] == -1
+                x[ind] = 0
+                nbVariablesLibre -= 1
+            end
+        end
+    end
+    return Solution(x, z)
+end
+
+function getfname(target)
+    # target : string := chemin + nom du repertoire ou se trouve les instances
+
+    # positionne le currentdirectory dans le repertoire cible
+    cd(joinpath(homedir(),target))
+
+    # recupere tous les fichiers se trouvant dans le repertoire data
+    allfiles = readdir()
+
+    # vecteur booleen qui marque les noms de fichiers valides
+    flag = trues(size(allfiles))
+
+    k=1
+    for f in allfiles
+        # traite chaque fichier du repertoire
+        if f[1] == "."
+            # fichier cache => supprimer
+            flag[k] = false
+        end
+        k = k+1
+    end
+
+    # extrait les noms valides et retourne le vecteur correspondant
+    finstances = allfiles[flag]
+    return finstances
+end
+
+function main()
+    gloutonBasique("Data/test.txt")
+    fnames = getfname("/home/lucas/Bureau/Dm/Data")
+	for fname in fnames
+		print("File : "*fname*" -> ")
+		sol = gloutonBasique(fname)
+		println(sol.z)
+	end
+end
+
+
+function exchangeKp(x::Vector{Int64}, z::Int64, prob::Problem, k::Int64, p::Int64)
+
+end
+
+function amelioration(sol::Solution, prob::Problem)
+	chgt::Bool = true
+	chgt21::Bool = true
+	chgt11::Bool = true
+	chgt10::Bool = true
+	x, z = sol.x, sol.z
+	nbchgt21 = 0
+	nbchgt11 = 0
+	nbchgt10 = 0
+
+	while chgt
+		chgt = false
+		# nbchgt21 = 0
+		while chgt21
+			chgt21 = true
+			newX, newZ = exchangeKp(x, z, prob, 2,1)
+			if z!=newZ
+				x = newX
+				z = newZ
+			else
+				chgt21 = false
+			end
+			# nbchgt21+=1
+		end
+		nbchgt11 = 0
+		while chgt11
+			chgt11 = true
+			newX, newZ = exchangeKp(x, z, prob, 1, 1)
+			if z!=newZ
+				x = newX
+				z = newZ
+			else
+				chgt11 = false
+			end
+			nbchgt11 +=1
+		end
+		nbchgt10 = 0
+		while chgt10
+			chgt10 = true
+			newX, newZ = exchangeKp(x, z, prob, 1, 0)
+			if z!=newZ
+				x = newX
+				z = newZ
+			else
+				chgt10 = false
+			end
+			nbchgt10 += 1
+		end
+
+		if nbchgt11 + nbchgt10 == 2
+			chgt = false
+		end
+	end
+	return Solution(x, z)
+end
